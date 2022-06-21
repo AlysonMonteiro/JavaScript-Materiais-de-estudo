dc1c4c7bb3aa56ef539dce31dd29dfa970e82b0c
Alyson Monteiro da silva <alisonmonteirotbl@gmail.com>
2022-06-21 15:26:41 -0300

    2 aulas

---
+++ js/aula10/index.js
@@ -0,0 +1,19 @@
+//String, Number, Undefined, Null, Boolean
+const nome = 'Luiz'; //String
+const nome1 = "Luiz"; //String
+const nome2 = `Luiz`; //String
+const num1 = 10; //number
+const num2 = 10.25; //number
+let nomeAluno; //UNDEFINED -> não aponta para local nenhum na memória
+let sobrenomeAlunu = null; //Nulo -> não aponta para local nenhum na memória
+const boolean = true || false ; //Boolean = true, false (logico)
+
+console.log(typeof nome, nome); //typeof
+ 
+let a = 2;
+const b = a;
+
+console.log(a, b);
+
+a = 3;
+console.log(a, b);
---
+++ js/aula11/index.js
@@ -0,0 +1,20 @@
+
+
+
+
+let contador = 0;
+
+ 
+
+
+
+contador += 2;
+contador += 2;
+contador += 2;
+contador *= 2;
+
+console.log(contador);
+
+const num1 = 10;
+const num2 = Number("5");
+console.log(num1 + num2);
