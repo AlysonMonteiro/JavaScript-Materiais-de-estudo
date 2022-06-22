7e36bf317d5032c62ea0fc85a6073bcabf30e088
Alyson Monteiro da silva <alisonmonteirotbl@gmail.com>
2022-06-22 11:02:45 -0300

    aula12

---
+++ js/aula12/script.js
@@ -0,0 +1,17 @@
+
+let confirmação = window.confirm('deseja fazer uma conta ?')
+
+if(confirmação === true){
+
+    num1 = window.prompt('digite um numero');
+    num2 = window.prompt('digite outro numero')
+    num1 = Number(num1);
+    num2 = Number(num2);
+    soma = num1 + num2;
+    window.alert(`sua soma deu ${soma}`);
+    window.alert('obrigado');
+
+}else{
+
+    window.alert("FODA-SE ENTÃO");
+}
