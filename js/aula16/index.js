79721a9bc72c51014bcfa7eaf74bd2b70e2fb576
Alyson Monteiro da silva <alisonmonteirotbl@gmail.com>
2022-07-13 22:04:44 -0300

    aula 16 functions

---
+++ js/aula16/index.js
@@ -0,0 +1,16 @@
+function soma(x = 1, y = 1) {
+  const resultado = x + y;
+  return resultado;
+}
+
+console.log(soma(8, 9));
+console.log(soma(10, 3));
+console.log(soma(6, 1));
+
+const raiz = function (n) {
+  return n ** 0.5;
+};
+
+console.log(raiz(9));
+console.log(raiz(80));
+console.log(raiz(90));
