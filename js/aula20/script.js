d012ca77e1bc4beef0622f0a0e44453550de3edd
Alyson Monteiro da silva <alisonmonteirotbl@gmail.com>
2022-07-18 19:34:46 -0300

    aula 19 e aula 20

---
+++ js/aula20/script.js
@@ -0,0 +1,31 @@
+/*
+operadores de comparação
+> maior que
+>= maior que ou igual a
+< menor que
+<= menor que ou igual a
+== igualdade
+=== igualdade estrita
+!= diferente
+!== diferente estrito
+*/
+
+const maiorQue =  10 > 5 ;
+const maiorQueOuIgualA = 10 >= 5;
+const menorQue = 10 < 5;
+const menorQueOuIgualA = 10 <= 5;
+const igualdade = 10 == 10;
+const igualdadeRestrital = 10 === 10;
+const diferente = 10 != '10';
+const diferenteRestrito = 10 !== '10';
+
+console.log(
+    maiorQue,
+    maiorQueOuIgualA,
+    menorQue,
+    menorQueOuIgualA,
+    igualdade,
+    igualdadeRestrital,
+    diferente,
+    diferenteRestrito
+    );
