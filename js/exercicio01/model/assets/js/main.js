2952a3667e16344fe5aa57ec05a779d95c7c8a9b
Alyson Monteiro da silva <alisonmonteirotbl@gmail.com>
2022-07-27 21:24:29 -0300

    exercicio completo

--- js/exercicio01/model/assets/js/main.js
+++ js/exercicio01/model/assets/js/main.js
@@ -1,15 +1,93 @@
-const formulario = document.querySelector(".formulario");
-const peso = formulario.querySelector(".peso").value;
-const altura = formulario.querySelector(".altura").value;
-const calculo = document.querySelector(".calc");
-const IMC = (peso / altura) * altura;
+//Capturar evento de submit do formulario
 
-const valorPeso = peso.value;
-const valorAltura = altura.value;
+const formulario = document.querySelector(".formulario");
+
 
 
 function retirarSubmit(evento) {
   evento.preventDefault();
+
+  //Captura dos dados dos inputs
+  const inputPeso = evento.target.querySelector(".peso");
+  const inputAltura = evento.target.querySelector(".altura");
+
+  const peso = Number(inputPeso.value);
+  const altura = Number(inputAltura.value);
+
+  //checagem para NaN
+  if (!peso) {
+    setResultado("Peso inválido", false);
+    return;
+  }
+
+  if (!altura) {
+    setResultado("Altura inválida", false);
+    return;
+  }
+  const imc = getImc(peso, altura);
+  const nivelImc = getNivelImc(imc);
+  
+
+  const msg = `Seu IMC é ${imc} (${nivelImc}).`;
+  setResultado(msg,true)
 }
 
+function getNivelImc(imc) {
+  const nivel = [
+    "Abaixo do peso",
+    "Peso normal",
+    "Sobrepeso",
+    "Obesidade grau 1",
+    "Obesidade grau 2",
+    "Obesidade grau 3",
+  ];
+
+  if (imc >= 39.9) {
+    return nivel[5];
+  }
+  if (imc >= 34.9) {
+    return nivel[4];
+  }
+  if (imc >= 29.9) {
+    return nivel[3];
+  }
+  if (imc >= 24.0) {
+    return nivel[2];
+  }
+  if (imc >= 18.5) {
+    return nivel[1];
+  }
+  if (imc < 18.5) {
+    return nivel[0];
+  }
+}
+
+function getImc(peso, altura) {
+  const imc = peso / altura ** 2;
+  return imc.toFixed(2);
+
+
+}
 formulario.addEventListener("submit", retirarSubmit);
+
+function setResultado(msg, isValid) {
+  const calculo = document.querySelector(".calc");
+  calculo.innerHTML = "";
+  const p = criaP();
+
+  if(isValid) {
+    p.classList.add("paragrafo_resultado");
+  }else{
+    p.classList.add('bad');
+  }
+
+  calculo.appendChild(p);
+  p.innerHTML = msg;
+}
+
+function criaP() {
+  const p = document.createElement("p");
+  return p;
+}
+
+criaP();
