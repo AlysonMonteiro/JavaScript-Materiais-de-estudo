5ac64bb4f5f33a972d3a8cf5edbe029335fa6bcf
Alyson Monteiro da silva <alisonmonteirotbl@gmail.com>
2022-08-16 19:34:09 -0300

    aula 32 e 33

---
+++ js_2/aula32/index.js
@@ -0,0 +1,29 @@
+// I = INDEX
+
+// for (let i = 0; i <= 50; i += 10) {
+//   console.log(`linha ${i}`);
+// }
+
+// for (let e = 50; e >= 30; e -= 2) {
+//   console.log(`linha ${e}`);
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//     const par = i % 2 === 0 ? 'par' : 'impar' ;
+//   console.log(i, par);
+// }
+
+const frutas = [
+  "maçã",
+  "pêra",
+  "Uva",
+  "melancia",
+  "mamão",
+  "cerreja",
+  "goiaba",
+];
+
+console.log(frutas.length);
+for (let i = 0; i < frutas.length; i++) {
+    console.log(`Índice ${i}`, frutas[i]);
+}
