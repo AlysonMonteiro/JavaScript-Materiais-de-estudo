9f47d9580a663a7790ee72973d9e0d3510c03f52
Alyson Monteiro da silva <alisonmonteirotbl@gmail.com>
2022-08-18 18:44:01 -0300

    aula 34 e 35

---
+++ js_2/aula35/index.js
@@ -0,0 +1,27 @@
+// const pessoa = {
+//     nome: 'pedro',
+//     sobrenome:'andre',
+//     idade:80
+// };
+// const chave = 'nome';
+// console.log(pessoa.nome);
+// console.log(pessoa[chave]);
+
+// for ( let chave in pessoa){
+//     console.log(chave, pessoa[chave]);
+// }
+
+// const frutas = ['pera','maça','uva'];
+// for ( let indice in frutas){
+//     console.log(frutas[indice]);
+// }
+
+// for ( let i = 0; i <= frutas.length; i++){
+//     console.log(frutas[i]);
+// }
+
+const numeros = ["pedra", "60", "gota", "feijão"];
+
+for (let indice in numeros) {
+  console.log(numeros[indice]);
+}
