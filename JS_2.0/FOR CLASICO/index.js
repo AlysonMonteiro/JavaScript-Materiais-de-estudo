17d556833192a7ff3bd5feba13c292e738e255fb
Alyson Monteiro da silva <alisonmonteirotbl@gmail.com>
2022-11-30 10:05:37 -0300

    2.0

---
+++ JS_2.0/FOR CLASICO/index.js
@@ -0,0 +1,15 @@
+//OBS01: dentro do FOR é necessario criar uma variavel de controle, normalmente chamada de "i" DE "INDEX".
+
+for(i = 0; i <= 10; i++){
+//  console.log(i);
+}
+
+//VARIAÇÕES POSSIVEIS
+
+for(i = 0; i <= 100; i += 50){
+//  console.log(i);
+};
+
+for(i = 0; i <= 10; i -= 4){
+    console.log(i);
+}
